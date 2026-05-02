0f375ae584c69230fb501767e4f3b85524478627
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2026-05-01 17:06:53 -0700

    BTA! RPG! Refactor from ExampleMod

--- src/main/java/purplemushroom/btarpg/BTARPG.java
+++ src/main/java/purplemushroom/btarpg/BTARPG.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package purplemushroom.btarpg;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -6,12 +6,12 @@ import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-	public static final String MOD_ID = "examplemod";
+public class BTARPG implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+	public static final String MOD_ID = "btarpg";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
-		LOGGER.info("ExampleMod initialized.");
+		LOGGER.info("BTA! RPG! initialized.");
 	}
 
 	@Override
