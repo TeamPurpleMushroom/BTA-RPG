a86893dceebcf795a2856d83a793bd915c31da32
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2026-05-01 19:03:51 -0700

    First item attempt

--- src/main/java/purplemushroom/btarpg/BTARPG.java
+++ src/main/java/purplemushroom/btarpg/BTARPG.java
@@ -1,8 +1,11 @@
 package purplemushroom.btarpg;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemDiscMusic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.ItemBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
@@ -20,8 +23,19 @@ public class BTARPG implements ModInitializer, RecipeEntrypoint, GameStartEntryp
 	@Override
 	public void initNamespaces() {}
 
+	public static Item HauntMuskie2Record;
+	private static int currentItemID = 15000;
+
+	ItemBuilder GenericItemBuilder = new ItemBuilder(BTARPG.MOD_ID);
+
+	private static int newItemID() {
+		return currentItemID++;
+	}
+
 	@Override
-	public void beforeGameStart() {}
+	public void beforeGameStart() {
+		//HauntMuskie2Record = GenericItemBuilder.build(new Item("ExampleItem", MOD_ID + ":ExampleItem", newItemID()));
+	}
 
 	@Override
 	public void afterGameStart() {}
