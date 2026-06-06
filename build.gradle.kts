9cef973c02bbc9fecaa325b9e1a9b1b35356145d
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2026-06-06 14:39:40 -0700

    Fixed accessWidener (now called "classTweaker")

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ group = modGroup.get()
 version = modVersion.get()
 loom {
 	customMinecraftMetadata.set("https://downloads.betterthanadventure.net/bta-client/${libs.versions.btaChannel.get()}/${libs.versions.bta.get()}/manifest.json")
+	accessWidenerPath = file("src/main/resources/btarpg.classtweaker")
 }
 repositories {
 	mavenCentral()
